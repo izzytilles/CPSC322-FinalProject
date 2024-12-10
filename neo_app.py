ac30c61929d1520180c4c82cf3a82583d62c6a17
Michael-CS-GU <157167984+Michael-CS-GU@users.noreply.github.com>
2024-12-10 11:33:10 -0800

    Add files via upload

--- neo_app.py
+++ neo_app.py
@@ -42,12 +42,12 @@ def index():
 @app.route("/predict")
 def predict():
     # parse an unseen instance from the query string -- in the request object
-    estimated_diameter_min = request.args.get(
-        "estimated_diameter_min"
+    estimated_diameter_min = int(
+        request.args.get("estimated_diameter_min")
     )  # default to None if no level
-    estimated_diameter_max = request.args.get("estimated_diameter_max")
-    relative_velocity = request.args.get("relative_velocity")
-    miss_distance = request.args.get("miss_distance")
+    estimated_diameter_max = int(request.args.get("estimated_diameter_max"))
+    relative_velocity = int(request.args.get("relative_velocity"))
+    miss_distance = int(request.args.get("miss_distance"))
     instance = [
         estimated_diameter_min,
         estimated_diameter_max,
@@ -65,6 +65,6 @@ if __name__ == "__main__":
     # header, tree = load_model()
     # print(header)
     # print(tree)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
     # TODO: when deploying app to production, set debug = False
     # Check host / port values
